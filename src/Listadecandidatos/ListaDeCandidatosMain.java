6d4863335851ee263cf430f57c829d73cff24a9f
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-17 00:22:03 -0300

    Inserindo atualizações de estetica e lógica

--- src/Listadecandidatos/ListaDeCandidatosMain.java
+++ src/Listadecandidatos/ListaDeCandidatosMain.java
@@ -1,8 +1,36 @@
 package Listadecandidatos;
 
+import java.util.Scanner;
+
 public class ListaDeCandidatosMain {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         ListaDeCandidatos candidatos = new ListaDeCandidatos();
+        int respostaDoUsuario = 0;
+
+        System.out.print("Você gostaria de adicionar algum candidato a sua lista ([1] - sim / [2] - não): ");
+        respostaDoUsuario = input.nextInt();
+
+        if (respostaDoUsuario == 1) {
+            do {
+                candidatos.adicionarCandidatos();
+                System.out.print("Você gostaria de adicionar mais algum candidato em sua lista? ([1] - sim [2] - não): ");
+                respostaDoUsuario = input.nextInt();
+
+            } while (respostaDoUsuario == 1);
+
+            candidatos.mostrarAlunos();
+            System.out.print("Você gostaria de remover algum candidato da sua lista? ([1] - sim ] / [2] - não):  ");
+            respostaDoUsuario = input.nextInt();
+            do {
+                candidatos.removerCandidato();
+                System.out.print("Você gostaria de remover mais algum item da sua lista? ([1] - sim / [2] - não): ");
+                respostaDoUsuario = input.nextInt();
+            } while (respostaDoUsuario == 1);
+        }
+        System.out.println("\nVolte sempre, agradecemos por ser a sua preferência <3 ");
+        System.out.println("A sua lista após as atualizações: " + candidatos.getCandidatos());
+
 
     }
 }
