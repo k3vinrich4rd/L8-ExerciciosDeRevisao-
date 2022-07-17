3750517f3f2f0cbe9112e37c20c2dd5a2e0df97a
k3vinrich4rd <kevinrichardve@gmail.com>
2022-07-17 00:31:46 -0300

    Inserindo classe recém criada

--- src/cardapioderestaurante/CardapioDeRestaurante.java
+++ src/cardapioderestaurante/CardapioDeRestaurante.java
@@ -1,2 +1,21 @@
-package cardapioderestaurante;public class CardapioDeRestaurante {
+package cardapioderestaurante;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class CardapioDeRestaurante {
+    private String nomeDoPrato;
+    private  List<String> ingredientesDoPrato = new ArrayList<>();
+    private HashMap<String, String> pratosDoRestaurante = new HashMap<>();
+    int respostasNumericas;
+    int quantidadeDePratos;
+
+    public CardapioDeRestaurante(String nomeDoPrato, List<String> ingredientesDoPrato, HashMap<String, String> pratosDoRestaurante, int respostasNumericas, int quantidadeDePratos) {
+        this.nomeDoPrato = nomeDoPrato;
+        this.ingredientesDoPrato = ingredientesDoPrato;
+        this.pratosDoRestaurante = pratosDoRestaurante;
+        this.respostasNumericas = respostasNumericas;
+        this.quantidadeDePratos = quantidadeDePratos;
+    }
 }
